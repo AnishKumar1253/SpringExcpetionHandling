bf370e9dcba1c0eeeb43b281ab651b130091daff
Anish Kumar <62415768+AnishKumar1253@users.noreply.github.com>
2023-01-03 14:24:36 +0530

    Add files via upload

---
+++ ExceptionValidAssignment/src/main/java/com/masai/app/ExceptionValidAssignmentApplication.java
@@ -0,0 +1,28 @@
+package com.masai.app;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import com.masai.app.entity.Post;
+
+@SpringBootApplication
+public class ExceptionValidAssignmentApplication {
+
+	public static void main(String[] args) {
+	SpringApplication.run(ExceptionValidAssignmentApplication.class, args);
+		System.out.println("Application has started running");
+		
+	
+	}
+	
+	@Bean
+	public LocalValidatorFactoryBean validator(MessageSource ms) {
+		LocalValidatorFactoryBean lvfb=new LocalValidatorFactoryBean();
+		lvfb.setValidationMessageSource(ms);
+		return lvfb;
+	}
+
+}
